1f4ff4d79597e496742fabd65733536648679d95
Philip Hayton <gotbadger@users.noreply.github.com>
2018-08-01 13:07:53 +0100

    move header

--- c/gigasecond/src/gigasecond.c
+++ c/gigasecond/src/gigasecond.c
@@ -1,4 +1,5 @@
 #include "gigasecond.h"
+#include <math.h>
 
 time_t gigasecond_after(time_t dt){
   return dt + pow(10,9);
--- c/gigasecond/src/gigasecond.h
+++ c/gigasecond/src/gigasecond.h
@@ -1,5 +1,4 @@
 #pragma once
 #include <time.h>
-#include <math.h>
 
 time_t gigasecond_after(time_t dt);
